135392c4ebda6721e67f4ddda488affbd5d97c36
Valentina <vliberonazuniga@gmail.com>
2026-01-19 15:02:37 -0300

    refactor: Remove BentoGrid, ProfileTile, ProjectTile, TechStackTile components and their exports from Bento module

--- src/components/bento/index.ts
+++
@@ -1,4 +0,0 @@
-export { BentoGrid, BentoTile } from './BentoGrid';
-export { ProfileTile } from './ProfileTile';
-export { TechStackTile } from './TechStackTile';
-export { ProjectTile } from './ProjectTile';
